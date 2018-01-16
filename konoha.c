1d6ca3b7104ac626fd28933c443b7b8ab7b5ac9d
NoNameA 774 <nonamea774@gmail.com>
2018-01-17 03:08:31 +0900

    ret any int from stdin(identical)

--- konoha.c
+++ konoha.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 
 int main(int argc, char** argv) {
+  int var;
+  scanf("%d", &var);
   printf("\t.text\n"
          "\t.global mymain\n"
          "mymain:\n"
-         "\tmov $42, %%eax\n"
-         "ret\n");
+         "\tmov $%d, %%eax\n"
+         "ret\n", var);
   return 0;
 }
