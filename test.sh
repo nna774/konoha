55869d67d3c7689f66976a91b3e77af1201671d6
NoNameA 774 <nonamea774@gmail.com>
2018-01-17 03:11:14 +0900

    divide compile step

--- test.sh
+++ test.sh
@@ -1,17 +1,23 @@
 #! /bin/bash -x
 
-test() {
-    expected="$1"
-    expr="$2"
-
-    echo "$expr" | ./konoha > tmp/out.s
+compile() {
+    echo "$1" | ./konoha > tmp/out.s
     if [ $? != 0 ]; then
+	echo "compilation fail"
 	exit -1
     fi
     gcc tmp/out.s driver.c -o tmp/a.out
     if [ $? != 0 ]; then
+	echo "gcc fail"
 	exit -1
     fi
+}
+
+test() {
+    expected="$1"
+    expr="$2"
+
+    compile "$expr"
     res=`./tmp/a.out`
     if [ "x$res" != "x$expected" ]; then
 	echo "Test failed: expected $expected, but got $res"
