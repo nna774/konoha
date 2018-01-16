53b162addb685aef98acced3a7bf4938cdab8d1a
NoNameA 774 <nonamea774@gmail.com>
2018-01-17 02:51:14 +0900

    fix test.sh to fail when gcc fail

--- test.sh
+++ test.sh
@@ -1,10 +1,13 @@
 #! /bin/sh -x
 
 ./konoha > tmp/out.s
-if [ ! $? ]; then
+if [ $? != 0 ]; then
     exit -1
 fi
 gcc tmp/out.s -o tmp/a.out
+if [ $? != 0 ]; then
+    exit -1
+fi
 ./tmp/a.out
 if [ $? != 42 ]; then
     exit -1
