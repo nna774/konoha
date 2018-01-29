aa9e623be5f1b42e245565ee531d558e5e20bfe9
Kugayama Nana <nonamea.774@gmail.com>
2018-01-29 16:27:39 +0900

    remove count_of_TYPE

--- src/enum.h
+++ src/enum.h
@@ -5,15 +5,15 @@
 
 void _setup_show_enum(char*, size_t*);
 
-#define _ENUM_COUNT_OF(Type) CONCAT(_ENUM_COUNT_OF_, Type)
+#define _NUMBER_OF_ARGS(...) (sizeof((int[]){__VA_ARGS__})/sizeof(int))
 
 #ifdef ENUM_SHOW_DEFINE
 #define ENUM_WITH_SHOW(Type, ...) \
-  enum Type { __VA_ARGS__, _ENUM_COUNT_OF(Type) };\
+  enum Type { __VA_ARGS__ };\
   typedef enum Type Type;\
   char const* CONCAT(show_, Type) (enum Type x) {\
     static char table[] = #__VA_ARGS__;\
-    static size_t map[_ENUM_COUNT_OF(Type)] = {0};\
+    static size_t map[_NUMBER_OF_ARGS(__VA_ARGS__)] = {0};\
     static bool is_first = 1;\
     if (is_first) {\
       _setup_show_enum(table, map);\
@@ -23,7 +23,7 @@ void _setup_show_enum(char*, size_t*);
   }
 #else
 #define ENUM_WITH_SHOW(Type, ...) \
-  enum Type { __VA_ARGS__, _ENUM_COUNT_OF(Type) };\
+  enum Type { __VA_ARGS__ };\
   typedef enum Type Type;\
   char const* CONCAT(show_, Type) (enum Type x);
 #endif
