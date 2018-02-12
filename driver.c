80a931da6997b0f1e8852ffdb9ebdb030c8a5a2d
Kugayama Nana <nonamea.774@gmail.com>
2018-02-13 04:11:56 +0900

    move function to self driver

--- driver.c
+++ driver.c
@@ -1,33 +1,5 @@
 #include <stdio.h>
 
-int return42() {
-  return 42;
-}
-
-int id(int n) {
-  return n;
-}
-
-int add(int n, int m) {
-  return n + m;
-}
-int add3(int n, int m, int o) {
-  return n + m + o;
-}
-int add4(int n, int m, int o, int p) {
-  return n + m + o + p;
-}
-int add5(int n, int m, int o, int p, int q) {
-  return n + m + o + p + q;
-}
-int add6(int n, int m, int o, int p, int q, int r) {
-  return n + m + o + p + q + r;
-}
-
-int mul(int n, int m) {
-  return n * m;
-}
-
 void print_int(int n) {
   printf("%d", n);
 }
